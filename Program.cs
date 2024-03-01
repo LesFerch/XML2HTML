4095843a9c398156f657f7f482b51d141af7ce34
LesFerch <79026235+LesFerch@users.noreply.github.com>
2024-03-01 10:51:20 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Xsl;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Convert XML files with XSL stylesheet to HTML");
+            Console.WriteLine("Usage: XML2HTML <file1.xml> <file2.xml> ...");
+            return;
+        }
+
+        foreach (var xmlFile in args)
+        {
+            try
+            {
+                string xslFileName = GetXslFileNameFromXml(xmlFile);
+                if (xslFileName == null)
+                {
+                    Console.WriteLine($"XSL filename not found in {xmlFile}");
+                    continue;
+                }
+
+                string xmlContents = File.ReadAllText(xmlFile);
+                string xslContents = File.ReadAllText(xslFileName);
+
+                string outputFileName = Path.GetFileNameWithoutExtension(xmlFile) + ".html";
+                TransformXml(xmlContents, xslContents, outputFileName);
+
+                Console.WriteLine($"Transformed [{xmlFile}] with [{xslFileName}] to make [{outputFileName}]");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error processing {xmlFile}: {ex.Message}");
+            }
+        }
+    }
+
+    static string GetXslFileNameFromXml(string xmlFile)
+    {
+        string[] lines = File.ReadAllLines(xmlFile);
+        foreach (string line in lines)
+        {
+            if (line.Contains("<?xml-stylesheet"))
+            {
+                int startIndex = line.IndexOf("href=\"") + "href=\"".Length;
+                int endIndex = line.IndexOf("\"", startIndex);
+                if (startIndex != -1 && endIndex != -1)
+                {
+                    return line.Substring(startIndex, endIndex - startIndex);
+                }
+            }
+        }
+        return null;
+    }
+
+    static void TransformXml(string xmlContent, string xslContent, string outputFileName)
+    {
+        var xmlDocument = new XmlDocument();
+        xmlDocument.LoadXml(xmlContent);
+
+        var xsltDocument = new XslCompiledTransform();
+        xsltDocument.Load(new XmlTextReader(new StringReader(xslContent)));
+
+        using (var memoryStream = new MemoryStream())
+        {
+            using (var xmlTextWriter = XmlWriter.Create(memoryStream, new XmlWriterSettings { OmitXmlDeclaration = true, Encoding = new UTF8Encoding(false), Indent = true }))
+            {
+                xsltDocument.Transform(xmlDocument, null, xmlTextWriter);
+            }
+
+            string htmlContent = Encoding.UTF8.GetString(memoryStream.ToArray());
+
+            htmlContent = InsertContentTypeMetaTag(htmlContent);
+
+            File.WriteAllText(outputFileName, htmlContent);
+        }
+    }
+    static string InsertContentTypeMetaTag(string htmlContent)
+    {
+        int headIndex = htmlContent.IndexOf("<head>");
+        if (headIndex != -1)
+        {
+            string metaTag = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">";
+            return htmlContent.Insert(headIndex + "<head>".Length, metaTag);
+        }
+        return htmlContent;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # XML2HTML
-Convert one or more XML files, with XSL stylesheet, to HTML
+
+Command line tool to convert one or more XML files, with XSL stylesheet, to HTML.
+
+The new file is saved to the same folder as the XML file with the same name but with an HTML extension.
+
+The tool can be used from the command line or via drag and drop.
+
+[![image](https://user-images.githubusercontent.com/79026235/152910441-59ba653c-5607-4f59-90c0-bc2851bf2688.png)Download the zip file](https://github.com/LesFerch/XML2HTML/releases/download/1.0.0/XML2HTML.zip)
+
+## How to Download and Run
+
+1. Download the zip file using the link above.
+2. Extract **XML2HTML.exe**.
+3. Right-click **XML2HTML.exe**, select Properties, check Unblock, and click OK.
+4. Optionally move **XML2HTML.exe** to the folder of your choice.
+5. Drag and drop XML files onto **XML2HTML.exe** to convert them to HTML. Or run it from the command line.
+6. If you skipped step 3, then, in the SmartScreen window, click More info and then Run anyway.
+
+**Note**: Some antivirus software may falsely detect the download as a virus. This can happen any time you download a new executable and may require extra steps to whitelist the file.
+
+[![image](https://user-images.githubusercontent.com/79026235/153264696-8ec747dd-37ec-4fc1-89a1-3d6ea3259a95.png)](https://github.com/LesFerch/XML2HTML)
